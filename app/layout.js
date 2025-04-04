104f4241c65edbe2d0fd4590f438dd3c3de05318
bilal-waleed <bilalwaleed246@gmail.com>
2025-04-05 04:41:40 +0500

    add icon

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,13 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "My Todo App",
+  description: "A simple and smart todo app",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
